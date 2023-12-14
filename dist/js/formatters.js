0aa2b4ab5856f4e0278c859103b090dee929f9ab
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-12-14 19:34:28 -0300

    :bulb: docs: Adição de comentários - functions - formatters

--- dist/js/formatters.js
+++ dist/js/formatters.js
@@ -1,6 +1,9 @@
+// FUNÇÃO PARA FORMATAR O VALOR DE NÚMERO PARA TEXTO (STRING)
 function formatarMoeda(valor) {
+    // FORMATAR A STRING EM PORTUGUÊS BRASILEIRO, ESTILO DE MOEDA, MOEDA BRASILEIRA
     return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 }
+// FUNÇÃO PARA FORMATAR A DATA DO TIPO DATA PARA TEXTO (STRING)
 function formatarData(data) {
     return data.toLocaleDateString("pt-br", {
         weekday: "long",

3b6927069c1231a9ac6366f53a4c45d1e5a912f3
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-12-14 19:30:28 -0300

    :bulb: docs: Adição de comentários - component - saldo

--- dist/js/saldo-component.js
+++ dist/js/saldo-component.js
@@ -1,9 +1,14 @@
+// REFERÊNCIA VARIÁVEL COM O SALDO DA CONTA
 let saldo = 3000;
+// SELEÇÃO DO VALOR DO INPUT COLETADO NO HTML
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+// SELEÇÃO DA DATA COLETADA NO INPUT HTML
 const elementoDataAcesso = document.querySelector(".block-saldo time");
+// SE O SALDO DA TRANSAÇÃO NÃO FOR NULO, FORMATE A MOEDA 
 if (elementoSaldo != null) {
     elementoSaldo.textContent = formatarMoeda(saldo);
 }
+// SE A DATA DA TRANSAÇÃO NÃO FOR NULO, FORMATE A NOVA DATA 
 if (elementoDataAcesso != null) {
     const dataAcesso = new Date();
     elementoDataAcesso.textContent = formatarData(dataAcesso);
